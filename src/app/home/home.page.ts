9d63baecb5c9147931c6a999cadac4f0088a193f
Alexandre Yudi <alexyud@hotmail.com>
2023-11-13 20:47:43 -0300

    Add main screen and basic functionalities

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,33 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public viewers: number = 0;
+  public viewersPerSecond: number = 6;
+  public money: number = 0;
+  public moneyPerSecond: number = 2;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.autoClicker();
+  }
+
+  addClick() {
+    this.viewers += 1;
+  }
+
+  addMoney() {
+    this.money += 1;
+  }
+
+  autoClicker() {
+    setInterval(() => {
+      this.viewers += this.viewersPerSecond;
+      if (this.viewers > 50) {
+        if (Math.floor(Math.random() * 10) > 7) this.addMoney();
+      }
+      
+    }, 400);
+  }
 
 }
